2d4b1d955fb19171c2c1e3514237f372279227e0
Mbelenga <mbelengaterence@gmail.com>
2024-07-25 01:21:44 +0300

    FIFO caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -7,17 +7,17 @@ from base_caching import BaseCaching
 class FIFOCache(BaseCaching):
     """ FIFOCache class """
     def __init__(self):
-        """ Initializes A FIFOCache """
+        """ initialize FIFO cache """
         super().__init__()
-    
+
     def put(self, key, item):
-        """ Add an item in the cache """
+        """ Add item using FIFO algo """
         if key is not None and item is not None:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 discarded_key = next(iter(self.cache_data))
                 del self.cache_data[discarded_key]
                 print("DISCARD: {}".format(discarded_key))
-                self.cache_data[key] = item
+            self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key """
