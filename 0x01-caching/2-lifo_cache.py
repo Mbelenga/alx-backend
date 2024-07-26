56a9e807129e6f1b50ccf8241c26a75d6079dda7
Mbelenga <mbelengaterence@gmail.com>
2024-07-27 02:01:23 +0300

    Lifo caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -16,7 +16,7 @@ class LIFOCache(BaseCaching):
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 discarded_key = list(self.cache_data.keys())[-1]
                 del self.cache_data[discarded_key]
-                print(f"DISCARD: {discarded_key}")
+                print("DISCARD: {}".format(discarded_key))
             self.cache_data[key] = item
 
     def get(self, key):
